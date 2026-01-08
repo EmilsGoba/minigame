04feeacb4dd1458dbe0b5f711453183872429992
EmilsGoba <ipa23.e.goba@vtdt.edu.lv>
2026-01-08 12:50:18 +0200

    sql

--- config.php
+++ config.php
@@ -10,3 +10,16 @@ return [
   "charset" => "utf8mb4"
   ]  
 ];
+
+/*
+return [
+  "database" => [
+  "host" => "localhost",
+  "port" => "3306",
+  "dbname" => "eklase",
+  "user" => "root",
+  "password" => "root",
+  "charset" => "utf8mb4"
+  ]  
+];
+*/
